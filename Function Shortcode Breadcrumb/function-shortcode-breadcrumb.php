b1dfff90eddd110977203fa0c90dd0f1d77c2089
Herson Samir Martinez <93807097+HrsamiM@users.noreply.github.com>
2021-12-06 22:54:31 -0600

    Hero Section
    Used hero section and feature image background, add shortcode breadcrumb.

---
+++ Function Shortcode Breadcrumb/function-shortcode-breadcrumb.php
@@ -0,0 +1,25 @@
+
+<?php
+//Function Shortcode Breadcrumb
+function breadcrumbs() {
+    $homePageTitle = get_the_title( get_option('page_on_front') );
+
+    if(!is_home()) {
+        echo '<nav class="breadcrumb">';
+        echo '<a class="breadLink" href="'.home_url('/').'">'.$homePageTitle.'</a><span class="divider"> / </span>';
+        if (is_category() || is_single()) {
+           the_category(' <span class="divider"> / </span> ');
+            if (is_single()) {
+                echo '<span class="divider"> / </span> ';
+                the_title();
+            }
+        } elseif (is_page()) {
+            the_title();
+        }
+        echo '</nav>';
+    }
+}
+add_shortcode( 'breadcrumbs', 'breadcrumbs' );
+
+//End Function Shortcode Breadcrumbs
+
---
+++ Function Shortcode Breadcrumb/hero-section-breadcrumb.php
@@ -0,0 +1,37 @@
+<?php
+/**
+* Template Name: Use hero-section & Breadcrumb
+*
+*/
+get_header(); 
+
+/*Var*/
+
+$headingTitle = get_the_title();
+$breadCrumbs = woocommerce_breadcrumb();
+?>
+
+<?php
+  	if ( has_post_thumbnail() ) {
+    	$imageBnnr = get_the_post_thumbnail_url();
+  	}
+	else {
+  		$imageBnnr = get_stylesheet_directory_uri().'/assets/media/bnner-df.jpg';
+	}
+?>
+  
+<div class="hero-section" style="background-image: url(<?php echo $imageBnnr; ?>);">
+<div class="container content-box">
+	<div class="row">
+		<div class="col-12">
+
+<?php echo '<h1 class="display-5 fw-bold lh-1 mb-3 heading">'.$headingTitle.'</h1>'; ?>
+<span class="text-center hrm-bcshrt"><?php do_shortcode('[breadcrumbs]'); ?></span>
+
+</div>
+</div>
+</div>
+</div>
+
+
+<?php get_footer(); ?>
