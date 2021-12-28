8f0930880c0fa1e16616807084ad92d5e95e15b2
Herson Samir Martinez <93807097+HrsamiM@users.noreply.github.com>
2021-12-28 15:05:00 -0500

    Update function-shortcode-breadcrumb.php

--- Function Shortcode Breadcrumb/function-shortcode-breadcrumb.php
+++ Function Shortcode Breadcrumb/function-shortcode-breadcrumb.php
@@ -1,24 +1,26 @@
 <?php
-//Function Shortcode Breadcrumb
+//Breadcrumbs Function
 function breadcrumbs() {
-    $homePageTitle = get_the_title( get_option('page_on_front') );
 
-    if(!is_home()) {
-        echo '<nav class="breadcrumb">';
-        echo '<a class="breadLink" href="'.esc_url(home_url('/')).'">'.$homePageTitle.'</a><span class="divider"> / </span>';
-        if (is_category() || is_single()) {
-           the_category(' <span class="divider"> / </span> ');
-            if (is_single()) {
-                echo '<span class="divider"> / </span> ';
-                the_title();
-            }
-        } elseif (is_page()) {
-            the_title();
-        }
-        echo '</nav>';
-    }
+  $homePageTitle = get_the_title( get_option('page_on_front') );
+
+  if(!is_home()) {
+      echo '<span class="text-center hrm-bcshrt"><nav class="breadcrumb">';
+      echo '<a class="breadLink" href="'.esc_url(home_url('/')).'">'.$homePageTitle.'</a><span class="divider"> / </span>';
+      if (is_category() || is_single()) {
+         the_category(' <span class="divider"> / </span> ');
+          if (is_single()) {
+              echo '<span class="divider"> / </span> ';
+              the_title();
+          }
+      } elseif (is_page()) {
+          the_title();
+      }
+      echo '</nav></span>';
+  }
 }
 add_shortcode( 'breadcrumbs', 'breadcrumbs' );
-//End Function Shortcode Breadcrumbs
+
+//End Breadcrumns Function
 
 ?>
